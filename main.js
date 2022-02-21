1b9301a98fee4f74a931c02a5418dfaf5eb02b65
Paul Russavage <Paul.Russavage@Henryscheinone.com>
2022-02-21 05:59:04 -0700

    adding 200 random stars in xyz space, plus a nice space backgrd pic for texture

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 
 // 2. Material - the wrapping paper/texture for an object
 // to see wireframe, no lighting needed
-// const material = new THREE.MeshBasicMaterial ({ color: 0xFF6347, wireframe: true,}); 
+// const material = new THREE.MeshBasicMaterial ({ color: 0xFF00FF, wireframe: true,}); 
 const material = new THREE.MeshStandardMaterial ({ color: 0xFF6347});
 
 // 3. Mesh - geometry + the material
@@ -46,17 +46,41 @@ scene.add(pointLight, ambientLight);
 //now some helpers yay
 const sphereSize = 1;
 const pointlightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+//gridhelper reminds me of battlezone grid haha
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(pointlightHelper, gridHelper);
 
+// listen to dom events w/ mouse and update camera accordingly
 const controls = new OrbitControls(camera, renderer.domElement);
+
+//TODO: learn how to do rainbows and make the color pink or purple - my daughter lol
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+
+  // randomly generate array w/ xyz coords to put a star in space somewhere
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar); //make 200 stars dang
+
+//add space backgrd
+const spaceTexture = new THREE.TextureLoader().load('space-unsplash.jpg');
+scene.background = spaceTexture;
 
 function animate () { // similar to a game loop in gamedev
   requestAnimationFrame(animate);
-  renderer.render(scene, camera)
+  
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
+  renderer.render(scene, camera)
 }
 
 animate();
